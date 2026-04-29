56254a089af9020dd420ecd313ba781157b0da69
singhayush61 <ayushofficialsv@gmail.com>
2026-04-29 21:52:42 +0530

    fix: Update AWS provider configuration in Terraform file

--- first_ec2.tf
+++ first_ec2.tf
@@ -9,11 +9,11 @@ terraform {
 
 resource "aws_instance" "terraform-ec2" {
 
-    ami           = "ami-0e12ffc2dd465f6e4"
-    instance_type = "t3.micro"
-    tags = {
-        Name = "my-first-terraform-ec2"
-    } 
-  
+  ami           = "ami-0e12ffc2dd465f6e4"
+  instance_type = "t3.micro"
+  tags = {
+    Name = "my-first-terraform-ec2"
+  }
+
 }
 
