7b5944d5c9f68d5ccfa7a19da8f5ac7a585d1350
singhayush61 <ayushofficialsv@gmail.com>
2026-04-29 23:21:12 +0530

    fix: ec2 instance run

--- first_ec2.tf
+++ first_ec2.tf
@@ -1,8 +1,8 @@
 
 provider "aws" {
-  region = "us-east-1"
+  region = "ap-south-1"
 }
-/*
+
 resource "aws_instance" "terraform-ec2" {
 
   ami           = "ami-0e12ffc2dd465f6e4"
@@ -12,8 +12,8 @@ resource "aws_instance" "terraform-ec2" {
   }
 
 }
-*/
 
-resource "aws_iam_user" "name" {
+
+/*resource "aws_iam_user" "name" {
   name = "terraform-user"
-}
+}*/
