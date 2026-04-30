c44a660781bd3fff37897b598d8a6f55f0a4f5ab
singhayush61 <ayushofficialsv@gmail.com>
2026-04-30 15:52:36 +0530

    feat:Add cross-reference attributes example

--- attributes.tf
+++ attributes.tf
@@ -1,4 +1,4 @@
-provider "aws" {
+/*provider "aws" {
   region = "ap-south-1"
 }
 
@@ -10,3 +10,4 @@ resource "aws_instance" "web" {
   ami           = "ami-0e12ffc2dd465f6e4"
   instance_type = "t2.micro"
 }
+*/
---
+++ cross-reference-attributes.tf
@@ -0,0 +1,21 @@
+
+provider "aws" {
+  region = "ap-south-1"
+}
+
+resource "aws_eip" "lb" {
+  domain   = "vpc"
+}
+
+resource "aws_security_group" "terraform-sg" {
+  name        = "attribute-sg"
+}
+
+resource "aws_vpc_security_group_ingress_rule" "terraform-sg-ipv4" {
+  security_group_id = aws_security_group.terraform-sg.id
+
+  cidr_ipv4   = "${aws_eip.lb.public_ip}/32"
+  from_port   = 443     
+  ip_protocol = "tcp"
+  to_port     = 443
+}
