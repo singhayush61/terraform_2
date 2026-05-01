b170e3a6cf424de62fbdef5ef7a3b940f88b7918
singhayush61 <ayushofficialsv@gmail.com>
2026-05-01 15:17:30 +0530

    feat: Add count-index.tf in terraform configuration file

---
+++ count-index.tf
@@ -0,0 +1,24 @@
+resource "aws_instance" "myec2" {
+    ami = "ami-0e12ffc2dd465f6e4"
+    instance_type = "t2.micro"
+    count = 3
+
+    tags = {
+      Name = "payments-system-${count.index}"
+    }
+}
+
+resource "aws_iam_user" "this" {
+  name = "payments-user-${count.index}"
+  count = 3
+}
+
+variable "users" {
+  type = list
+  default = ["alice", "bob", "johncorner","james","mrA"]
+}
+
+resource "aws_iam_user" "that" {
+  name = var.users[count.index]
+  count = 3
+}
--- count.tf
+++ count.tf
@@ -1,3 +1,4 @@
+/*
 resource "aws_instance" "myec2" {
     ami = "ami-00c39f71452c08778"
     instance_type = "t2.micro"
@@ -12,3 +13,4 @@ resource "aws_iam_user" "this" {
   name = "payments-user"
   count = 3
 }
+*/
